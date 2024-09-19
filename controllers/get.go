d5ae13ede67040789f5f8fcea55f7f150147dbbd
Syahrul Ramadhan <153973943+syrlramadhan@users.noreply.github.com>
2024-09-19 23:23:54 +0800

    Update get.go

--- controllers/get.go
+++ controllers/get.go
@@ -27,33 +27,34 @@ func AmbilHandler(w http.ResponseWriter, r *http.Request) {
 	if nama != n || kunci != k {
 		http.Error(w, `{"message":"Tidak memiliki akses!"}`, http.StatusUnauthorized)
 		return
-	}
-
-	var pendaftar []models.Pendaftar
-	config.DB.Find(&pendaftar)
-
-	data := make(map[string]models.Pendaftar)
-	for _, p := range pendaftar {
-		key := fmt.Sprintf("%d", p.ID)
-		data[key] = models.Pendaftar{
-			NamaLengkap:   p.NamaLengkap,
-			Email:         p.Email,
-			NoTelp:        p.NoTelp,
-			BuktiTransfer: "https://pendaftaran-coc-api-production.up.railway.app/static/" + p.BuktiTransfer,
+	} else {	
+		var pendaftar []models.Pendaftar
+		config.DB.Find(&pendaftar)
+	
+		data := make(map[string]models.Pendaftar)
+		for _, p := range pendaftar {
+			key := fmt.Sprintf("%d", p.ID)
+			data[key] = models.Pendaftar{
+				NamaLengkap:   p.NamaLengkap,
+				Email:         p.Email,
+				NoTelp:        p.NoTelp,
+				BuktiTransfer: "https://pendaftaran-coc-api-production.up.railway.app/static/" + p.BuktiTransfer,
+			}
 		}
+	
+		response := Response{
+			Message: "success",
+			Data:    data,
+		}
+	
+		jsonData, err := json.MarshalIndent(response, "", "    ")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jsonData)
 	}
 
-	response := Response{
-		Message: "success",
-		Data:    data,
-	}
-
-	jsonData, err := json.MarshalIndent(response, "", "    ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonData)
 }
