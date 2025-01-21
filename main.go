9e838fce43486956a54156ca51a021f6c7d3a021
Syahrul Ramadhan <153973943+syrlramadhan@users.noreply.github.com>
2025-01-21 15:02:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://coconutopenclass.vercel.app")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
